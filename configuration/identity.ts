fa5cc3d739338dcbcef0c0ece4d58c2caa08caa6
Heeba-S <104732743+Heeba-S@users.noreply.github.com>
2025-03-03 15:30:07 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Heeba`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `EventPlanner`;
+export const AI_NAME: string = `HalalBot`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
