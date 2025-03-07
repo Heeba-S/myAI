9206fa883fe05b99f51b7958f9d8cc7c36a05ae4
Heeba-S <104732743+Heeba-S@users.noreply.github.com>
2025-03-06 21:44:05 -0500

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,7 +1,7 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant. I'm here to help you find the best halal spots in the Chapel Hill and Durham areas.`;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant. I'm here to help you find the best halal spots on and around UNC's campus! So tell me, what cuisine are you in the mood for?`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
-export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
+export const WORD_BREAK_MESSAGE: string = `Looks like we're having a great conversation! Give me some time to digest our discussion and I'll be happy to get back to you soon!`;
 export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
