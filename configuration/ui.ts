8ac35840ff1eddbdc223c322f57c0660269eea38
Heeba-S <104732743+Heeba-S@users.noreply.github.com>
2025-03-12 23:15:05 -0400

    Update ui.ts
    changed message placeholder

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,7 +1,7 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `HalalBot`;
-export const MESSAGE_PLACEHOLDER: string = `Let's find you some good halal food!`;
+export const MESSAGE_PLACEHOLDER: string = `How can I help you?`;
 export const FOOTER_MESSAGE: string = `Customized AI by Heeba`;
 export const CLEAR_BUTTON_TEXT: string = `Start a new chat`;
 export const PAGE_TITLE: string = `HalalBot.AI`;
